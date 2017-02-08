eb21cf36cdff5b6c692f6d298e0a2c585f0fdd2a
Sarkanyolo <gyuri.deak@gmail.com>
2017-02-08 14:17:48 +0100

    Add getDictionary and Merge functions

--- DBConn.cs
+++ DBConn.cs
@@ -142,6 +142,36 @@
             }
         }
 
+        public static System.Collections.Generic.Dictionary<string, string> getDictionary(string distinctSQLWithTwoColumns) {
+            using (var dt = myDB.GetDataTable(distinctSQLWithTwoColumns)) {
+                var dict = new System.Collections.Generic.Dictionary<string, string>();
+                for(int i = 0; i < dt.Rows.Count; i++) {
+                    if(dt.Rows[i][0] == DBNull.Value || dt.Rows[i][1] == DBNull.Value) {
+                        try {
+                            dict.Add(dt.Rows[i][0].ToString(), dt.Rows[i][1].ToString());
+                        } catch {
+                            // Elem cannot be inserted
+                        }
+                    }
+                }
+                return dict;
+            }
+        }
+
+        public static System.Collections.Generic.Dictionary<string, string> getDictionary(string table, string keyColumn, string valueColumn) {
+            using (var dt = myDB.GetDataTable(string.Format("SELECT DISTINCT {0}, {1} FROM {2} WHERE {0} IS NOT NULL AND {1} IS NOT NULL", keyColumn, valueColumn, table))) {
+                var dict = new System.Collections.Generic.Dictionary<string, string>();
+                for (int i = 0; i < dt.Rows.Count; i++) {
+                    try {
+                        dict.Add(dt.Rows[i][0].ToString(), dt.Rows[i][1].ToString());
+                    } catch {
+                        // Elem cannot be inserted
+                    }
+                }
+                return dict;
+            }
+        }
+
         public static void Execute(string sql) {
             myDB.Execute(sql);
         }
@@ -165,6 +195,74 @@
             }
         }
 
+        public static bool Exists(string table, string clause) {
+            return GetResultAsInt(string.Format("SELECT COUNT(*) FROM {0} WHERE {1}",table,clause)) > 0;
+        }
+
+        /// <summary>
+        /// If a table cotains a specific data update it, if it doesn't insert
+        /// </summary>
+        /// <param name="table">The table's name</param>
+        /// <param name="Values">List of columns and values we want to update or insert</param>
+        /// <param name="OnClause">Conditions to decide if the table contains the data(from the values list)</param>
+        public static void Merge(string table, Dictionary<string, string> Values, HashSet<string> OnClause, HashSet<string> NeedToUpdate = null) {
+            string mergesql = "MERGE INTO {0} a USING (SELECT {1} FROM DUAL) b ON ({2}) {3} WHEN NOT MATCHED THEN INSERT ({4}) VALUES ({5})";
+            string s = string.Empty, s1 = string.Empty, s2 = string.Empty, s3 = string.Empty, s4 = string.Empty;
+            try {
+                if (Values == null || Values.Count == 0) {
+                    throw new Exception("Values cannot be null or empty!");
+                }
+                if (Values.Where(v => v.Key.Trim() == string.Empty || v.Value.Trim() == string.Empty).Count() > 0) {
+                    throw new Exception("Values cannot contain empty string!");
+                }
+                if (OnClause == null || OnClause.Count == 0) {
+                    throw new Exception("OnClause cannot be null or empty!");
+                }
+                if ((OnClause.Where(o => o.Trim() == string.Empty).Count() > 0)) {
+                    throw new Exception("OnClause cannot contain empty string!");
+                }
+                if (Values.Where(v => OnClause.Contains(v.Key)).Count() != OnClause.Count()) {
+                    throw new IndexOutOfRangeException("Onclause contains item not existing in Values!");
+                }
+                if (NeedToUpdate != null) {
+                    if (Values.Where(v => NeedToUpdate.Contains(v.Key)).Count() != NeedToUpdate.Count()) {
+                        throw new IndexOutOfRangeException("NeedToUpdate contains item not existing in Values!");
+                    } else {
+                        if ((NeedToUpdate.Where(o => o.Trim() == string.Empty).Count() > 0)) {
+                            throw new Exception("NeedToUpdate cannot contain empty string!");
+                        }
+                    }
+                }
+                foreach (var t in Values) {
+                    if (t.Value.ToUpper() == "SYSDATE") {
+                        s += string.Format("SYSDATE as {0},", t.Key);
+                    } else {
+                        s += string.Format("'{0}' as {1},", t.Value, t.Key);
+                    }
+                    if (OnClause.Contains(t.Key)) s1 += string.Format("a.{0} = b.{0} AND ", t.Key);
+                    if (!OnClause.Contains(t.Key)) {
+                        if (NeedToUpdate == null) {
+                            s2 += string.Format("a.{0} = b.{0},", t.Key);
+                        } else if (NeedToUpdate.Contains(t.Key)) {
+                            s2 += string.Format("a.{0} = b.{0},", t.Key);
+                        }
+                    }
+                    s3 += t.Key + ",";
+                    s4 += string.Format("b.{0},", t.Key);
+                }
+                s = s.Substring(0, s.Length - 1);
+                s1 = s1.Substring(0, s1.LastIndexOf("AND"));
+                if (!string.IsNullOrEmpty(s2)) s2 = "WHEN MATCHED THEN UPDATE SET" + s2.Substring(0, s2.Length - 1);
+                if (!string.IsNullOrEmpty(s3)) s3 = s3.Substring(0, s3.Length - 1);
+                if (!string.IsNullOrEmpty(s4)) s4 = s4.Substring(0, s4.Length - 1);
+
+                mergesql = string.Format(mergesql, table, s, s1, s2, s3, s4);
+                Execute(mergesql);
+            } catch (Exception) {
+                return;
+            }
+        }
+
         public sealed class SP {
             private OracleParameterCollection opc;
             private OracleParameter output;
@@ -177,7 +275,7 @@
                 opc.Add(output);
             }
 
-            public SP Add(string name, string value) {
+            public SP Add(string name, string value = "") {
                 opc.Add(new OracleParameter(name, value));
                 return this;
             }
@@ -186,6 +284,8 @@
                 return myDB.call_sp(spname, opc);
             }
         }
+
+        public static DateTime Sysdate { get { return DateTime.ParseExact(GetResultAsStr("SELECT to_char(sysdate,'yyyy/MM/dd HH24:MI:SS') FROM dual"), "yyyy/MM/dd HH:mm:ss", System.Globalization.CultureInfo.CreateSpecificCulture("en-US")); } }
 
         private DB() { }
     }
